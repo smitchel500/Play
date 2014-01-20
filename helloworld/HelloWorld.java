e4e9e017aaa563b845cf95c14a005e7b5af1dff3
Eddie Roache <eroache@example.com>
2014-01-19 23:57:48 -0500

    changed HelloWorld to 'hi'

--- helloworld/HelloWorld.java
+++ helloworld/HelloWorld.java
@@ -16,7 +16,6 @@ public class HelloWorld {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hi World!");
     }
-    
 }
